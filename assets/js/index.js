341b8148af293f39613bf965f2bc571b84b805c2
WilliamDinh <hoangdinh2411@gmail.com>
2021-10-27 17:25:10 +0200

    the layout how website looks like

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,8 @@ const $$ = document.querySelectorAll.bind(document)
 
 const helsingborgMain = $('#helsingborg');
 const acceptCookieContainer = $('#accept-cookie')
+const frontPage = $('#front-page')
+const mainPage = $('#main')
 const logo = $('#logo')
 const acceptBtn = $('.btn-accept')
 const policyBtn = $('.btn-policy')
@@ -10,24 +12,28 @@ const COOKIE_ACCEPTED = "cookie_accepted"
 let acceptedCookie = localStorage.getItem(COOKIE_ACCEPTED) || ""
 
 
-window.onload = function () {
+
+//Click on logo , show up accept cookie
+
+window.onload = () => {
     if (acceptedCookie != '') {
         helsingborgMain.classList.add('accepted')
 
     } else {
-        //Click on logo , show up accept cookie
-
-        logo.onclick = () => {
-            acceptCookieContainer.style.transform = 'translateY(0)';
-        }
-
-        //Click on Accept button, hide accept cookie
+        acceptCookieContainer.style.transform = 'translateY(0)';
         acceptBtn.addEventListener('click', () => {
             helsingborgMain.classList.add('accepted')
             localStorage.setItem(COOKIE_ACCEPTED, 'accepted')
         })
     }
+
 }
+
+logo.addEventListener('click', () => {
+    frontPage.style.display = 'none'
+    mainPage.style.display = 'block'
+})
+
 
 
 
