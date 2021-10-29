2ca0b374b40d29f1f6d538dc6363501832f82dff
WilliamDinh <hoangdinh2411@gmail.com>
2021-10-29 17:20:15 +0200

    Code was checked on the first week , good feedback from Dimitrji

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,9 +36,9 @@ logo.addEventListener('click', () => {
 })
 
 window.onscroll = () => {
-    if(window.scrollY >=200){
+    if (window.scrollY >= 200) {
         header.classList.add('follow')
-    }else{
+    } else {
         header.classList.remove('follow')
 
     }
@@ -53,27 +53,30 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // slideIn  bfgdex++;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000)
+
 }
 
 
