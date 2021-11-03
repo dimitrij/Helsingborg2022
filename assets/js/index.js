1c0a77fc4bbbb1644bf5ebbd7204a7ce837999f6
Maja <maja.pau99@gmail.com>
2021-11-03 11:38:01 +0100

    footer component

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,39 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-const acceptCookieContainer = $('#accept-cookie');
+const helsingborgMain = $('#helsingborg');
+const acceptCookieContainer = $('#accept-cookie')
+const frontPage = $('#front-page')
+const mainPage = $('#main')
+const logo = $('#logo')
 const acceptBtn = $('.btn-accept')
 const policyBtn = $('.btn-policy')
+const COOKIE_ACCEPTED = "cookie_accepted"
+let acceptedCookie = localStorage.getItem(COOKIE_ACCEPTED) || ""
 
-acceptBtn.addEventListener('click',()=>{
-    acceptCookieContainer.classList.add('accepted')
+
+
+//Click on logo , show up accept cookie
+
+window.onload = () => {
+    if (acceptedCookie != '') {
+        helsingborgMain.classList.add('accepted')
+
+    } else {
+        acceptCookieContainer.style.transform = 'translateY(0)';
+        acceptBtn.addEventListener('click', () => {
+            helsingborgMain.classList.add('accepted')
+            localStorage.setItem(COOKIE_ACCEPTED, 'accepted')
+        })
+    }
+
+}
+
+logo.addEventListener('click', () => {
+    frontPage.style.display = 'none'
+    mainPage.style.display = 'block'
 })
+
+
+
+
