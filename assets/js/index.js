833285dcbd2cd77d032f470d294110368766fcac
Niklas Wendel <88709975+Niwen85@users.noreply.github.com>
2021-11-16 14:52:24 +0100

    update

---
+++ assets/js/index.js
@@ -0,0 +1,86 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const helsingborgMain = $('#helsingborg');
+const acceptCookieContainer = $('#accept-cookie')
+const frontPage = $('#front-page')
+const mainPage = $('#main')
+const logo = $('#logo')
+const acceptBtn = $('.btn-accept')
+const policyBtn = $('.btn-policy')
+const header = $('header')
+const COOKIE_ACCEPTED = "cookie_accepted"
+let acceptedCookie = localStorage.getItem(COOKIE_ACCEPTED) || ""
+
+
+
+//Click on logo , show up accept cookie
+
+window.onload = () => {
+    if (acceptedCookie != '') {
+        helsingborgMain.classList.add('accepted')
+
+    } else {
+        acceptCookieContainer.style.transform = 'translateY(0)';
+        acceptBtn.addEventListener('click', () => {
+            helsingborgMain.classList.add('accepted')
+            localStorage.setItem(COOKIE_ACCEPTED, 'accepted')
+        })
+    }
+
+}
+
+logo.addEventListener('click', () => {
+    frontPage.style.display = 'none'
+    mainPage.style.display = 'block'
+})
+
+window.onscroll = () => {
+    if (window.scrollY >= 200) {
+        header.classList.add('follow')
+    } else {
+        header.classList.remove('follow')
+
+    }
+}
+
+
+//menubar
+document.querySelector(".menubar").addEventListener("click", function () {
+    document.querySelector(".menubar").classList.toggle('open')
+})
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    // slideIn  bfgdex++;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000)
+
+}
+
+
+
+
+
+
